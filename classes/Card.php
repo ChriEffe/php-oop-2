1a315da2606cfa402ff364bce4d63ccb4bb9d8ca
ChriEffe <christianferrazzano93@gmail.com>
2022-02-16 15:25:06 +0100

    newUser

--- classes/Card.php
+++ classes/Card.php
@@ -1,6 +1,6 @@
 <?php 
 
-class Product
+class Card
 {
     protected $accountHolder;
     protected $cardNumber;
@@ -12,6 +12,86 @@ class Product
         $this->cardNumber = $cardNumber;
         $this->expDate = $expDate;
         $this->cvc = $cvc;
+    }
+
+    /**
+     * Get the value of accountHolder
+     */ 
+    public function getAccountHolder()
+    {
+        return $this->accountHolder;
+    }
+
+    /**
+     * Set the value of accountHolder
+     *
+     * @return  self
+     */ 
+    public function setAccountHolder($accountHolder)
+    {
+        $this->accountHolder = $accountHolder;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cardNumber
+     */ 
+    public function getCardNumber()
+    {
+        return $this->cardNumber;
+    }
+
+    /**
+     * Set the value of cardNumber
+     *
+     * @return  self
+     */ 
+    public function setCardNumber($cardNumber)
+    {
+        $this->cardNumber = $cardNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of expDate
+     */ 
+    public function getExpDate()
+    {
+        return $this->expDate;
+    }
+
+    /**
+     * Set the value of expDate
+     *
+     * @return  self
+     */ 
+    public function setExpDate($expDate)
+    {
+        $this->expDate = $expDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cvc
+     */ 
+    public function getCvc()
+    {
+        return $this->cvc;
+    }
+
+    /**
+     * Set the value of cvc
+     *
+     * @return  self
+     */ 
+    public function setCvc($cvc)
+    {
+        $this->cvc = $cvc;
+
+        return $this;
     }
 }
 
--- index.php
+++ index.php
@@ -3,4 +3,13 @@
 require_once __DIR__ ."/classes/Product.php";
 require_once __DIR__ ."/classes/User.php";
 
+try {
+    $newUser = new User ("Christian", "Ferrazzano", "christian@mail.com", "via delle vie", new Card("Christian Ferrazzano", "65461516511", "12/22", "666"));
+
+    var_dump($newUser);
+    
+} catch (\Throwable $th) {
+    //throw $th;
+}
+
 ?>
