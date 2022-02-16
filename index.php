774d5b8eb834e84de784583b8b017f69020d416f
ChriEffe <christianferrazzano93@gmail.com>
2022-02-16 17:21:35 +0100

    non stampa dati carta

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once __DIR__ ."/classes/User.php";
 
 try {
     $newUser = new User ("Christian", "Ferrazzano", "christian@mail.com", "28", "via delle vie", new Card("Christian Ferrazzano", "65461516511", "12/22", "666"));
-    
+
     $newUser->setLevel("gold");
 
     $newProduct = new Product("Jordan 1", 210.00);
@@ -19,3 +19,24 @@ try {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><h1>Utente</h1></li>
+        <li>Nome: <? echo $newUser->getName() ?></li>
+        <li>Cognome: <? echo $newUser->getLastName() ?></li>
+        <li>Email: <? echo $newUser->getEmail() ?></li>
+        <li>Età: <? echo $newUser->getAge() ?></li>
+        <li>Indirizzo: <? echo $newUser->getAddress() ?></li>
+        <li>Sconto: %<? echo $newUser->getLevel() ?></li>
+    </ul>
+</body>
+</html>
